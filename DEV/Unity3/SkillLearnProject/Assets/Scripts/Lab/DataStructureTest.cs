179a9ead57f1bf6d927076965bc18700e52b9326
starstarenq <starstarenq@naver.com>
2026-09-22 19:52:36 +0900

    20260922

---
+++ DEV/Unity3/SkillLearnProject/Assets/Scripts/Lab/DataStructureTest.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DataStructureTest : MonoBehaviour
+{
+    List<CardData> AllCardData = new List<CardData>();
+
+    public CardData[] CardDatas;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
